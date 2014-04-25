7e1b88f34eb31d8b3a2f147364e6011197b21eee
Dominic Modalek <dominic@modalek.ch>
2014-04-25 16:29:07 +0200

    Update gruntfile

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<? include 'php/config.php'; ?>
+<? include 'php/app-env.php'; ?>
 <? include 'php/helper.php'; ?>
 <? include 'php/terrific.php'; ?>
 
@@ -38,63 +38,59 @@
 <div class="page-wrapper">
 
 	<header class="page-header" role="banner">
-
 		<?php echo module('logo') ?>
 		<?php echo module('main-nav') ?>
-
 	</header>
 
-	<main class="page-main base" role="main">
+	<div class="page-main base" role="main">
 
-		<div class="content">
+		<h2>Heading H2</h2>
 
-			<h2>Heading H2</h2>
+		<h3>Heading H3</h3>
+		<h4>Heading H4</h4>
 
-			<h3>Heading H3</h3>
-			<h4>Heading H4</h4>
+		<p>A paragraph with a <a href="#">Link</a> and some more text. A paragraph with a <a href="#">Link</a> and
+			some more text. A paragraph with a <a href="#">Link</a> and some more text. A paragraph with a
+			<a href="#">Link</a> and some more text. A paragraph with a <a href="#">Link</a> and some more text.
+			A paragraph with a <a href="#">Link</a> and some more text. A paragraph with a <a href="#">Link</a> and
+			some more text. A paragraph with a <a href="#">Link</a> and some more text.</p>
 
-			<p>A paragraph with a <a href="#">Link</a> and some more text. A paragraph with a <a href="#">Link</a> and
-				some more text. A paragraph with a <a href="#">Link</a> and some more text. A paragraph with a
-				<a href="#">Link</a> and some more text. A paragraph with a <a href="#">Link</a> and some more text.
-				A paragraph with a <a href="#">Link</a> and some more text. A paragraph with a <a href="#">Link</a> and
-				some more text. A paragraph with a <a href="#">Link</a> and some more text.</p>
+		<ul>
+			<li>List Item</li>
+			<li>List Item</li>
+			<li>
+				<ul>
+					<li>Nested List Item</li>
+					<li>Nested List Item</li>
+				</ul>
+			</li>
+		</ul>
 
-			<ul>
-				<li>List Item</li>
-				<li>List Item</li>
-				<li>
-					<ul>
-						<li>Nested List Item</li>
-						<li>Nested List Item</li>
-					</ul>
-				</li>
-			</ul>
+		<h4>Grid Example</h4>
 
-			<h4>Grid Example</h4>
-			<p>Use the grid via .row and .col mixins or directly in the Markup as HTML classes.</p>
-			<div class="debug-grid">
-				<div class="row">
-					<div class="col-6">
-						<div class="row">
-							<div class="col-3"></div>
-							<div class="col-3"></div>
-							<div class="col-3"></div>
-							<div class="col-3"></div>
-						</div>
+		<p>Use the grid via .row and .col mixins or directly in the Markup as HTML classes.</p>
+
+		<div class="debug-grid">
+			<div class="row">
+				<div class="col-6">
+					<div class="row">
+						<div class="col-3"></div>
+						<div class="col-3"></div>
+						<div class="col-3"></div>
+						<div class="col-3"></div>
 					</div>
-					<div class="col-6">
-						<div class="row">
-							<div class="col-4"></div>
-							<div class="col-4"></div>
-							<div class="col-4"></div>
-						</div>
+				</div>
+				<div class="col-6">
+					<div class="row">
+						<div class="col-4"></div>
+						<div class="col-4"></div>
+						<div class="col-4"></div>
 					</div>
 				</div>
 			</div>
-
 		</div>
 
-	</main>
+	</div>
 
 	<footer class="page-footer" role="contentinfo">
 		<?php echo module('footer-links') ?>
