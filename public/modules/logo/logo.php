6106d6d3b298de3e5931666126cbcfc047d52191
Dominic Modalek <dominic@modalek.ch>
2014-04-25 10:13:31 +0200

    Update Base Theme

--- public/modules/logo/logo.php
+++ public/modules/logo/logo.php
@@ -1,6 +1,6 @@
 <div class="inner">
 	<a class="logo-link" href="/">
-		<img src="img/example-logo.png" width="300" height="225" alt="Logo" class="flex">
+		<img src="img/example-logo.png" width="300" height="225" alt="Logo" class="img flex">
+		<h1 class="title">Awesome Kickstart</h1>
 	</a>
-	<h1 class="title">Awesome Kickstart</h1>
 </div>
